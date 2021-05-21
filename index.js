1d1d9d46adfc06ac1ebcd715edd22101e0b9e9ab
wilfried <wilfried.odorico@gmail.com>
2021-05-21 16:21:07 +0200

    ajout de la méthode bascule rond croix

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const game = document.querySelector(".game");
-const caseUnique = document.querySelectorAll(".card");
+const cardList = document.querySelectorAll(".card");
 const tableGame = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let tab = [];
 const rond = "O";
 const croix = "X";
-const card = `<div class='card'></div>`;
+let isCheck = true;
 
-for(const item of caseUnique) {
-  item.addEventListener('click', (e) => {
-    item.innerHTML = croix;
+for (const item of cardList) {
+  item.addEventListener("click", (e) => {
+    if (item.innerText === "") {
+      isCheck ? (isCheck = false) : (isCheck = true);
+      isCheck ? (item.innerHTML = croix) : (item.innerHTML = rond);
+    }
     
-    console.log("-----", caseUnique[1]);
-    console.log("click", e.target.innerText);
-  })
-  console.log("item", item)
+  });
+
+  
 }
-
-console.log(caseUnique)
-
-
