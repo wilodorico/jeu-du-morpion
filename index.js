96f954d44551ec2221d51a7476b6deae8381f6c1
wilfried <wilfried.odorico@gmail.com>
2021-05-26 21:52:55 +0200

    ajout des condition de verif combiWin plus message du gagnant

--- index.js
+++ index.js
@@ -1,36 +1,51 @@
 const game = document.querySelector(".game");
 const cardList = document.querySelectorAll(".card");
+const message = document.querySelector('.message')
+const playerOne = "O";
+const playerTwo = "X";
+let gagne = false;
+let isCheck = false;
 const tableGame = ["", "", "", "", "", "", "", "", ""];
-const rond = "O";
-const croix = "X";
-let isCheck = true;
 const combiWin = [
-  [0,1,2],
-  [0,3,6],
-  [0,4,8],
-  [1,4,7],
-  [2,5,8],
-  [2,4,6],
-  [3,4,5],
-  [6,7,8]
-]
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
 
 for (const item of cardList) {
   item.addEventListener("click", (e) => {
     if (item.innerText === "") {
       isCheck ? (isCheck = false) : (isCheck = true);
     }
-    isCheck ? (
-        item.innerHTML = croix,
-        tableGame[item.dataset.id] = croix
-      ) : (
-        item.innerHTML = rond,
-        tableGame[item.dataset.id] = rond
-      );
-      
-      
+    
+    isCheck
+      ? ((item.innerHTML = playerTwo),
+        (item.style.color = "red"),
+        (tableGame[item.dataset.id] = playerTwo))
+      : ((item.innerHTML = playerOne),
+        (item.style.color = "orange"),
+        (tableGame[item.dataset.id] = playerOne));
+    console.log("tableGame :>> ", tableGame);
+
+    for (let index of combiWin) {
+      let val1 = tableGame[index[0]];
+      let val2 = tableGame[index[1]];
+      let val3 = tableGame[index[2]];
+      if (val1 === "" || val2 === "" || val3 === "") {
+        continue;
+      }
+      if (val1 === val2 && val2 === val3) {
+        val1 === "X" ? 
+        (message.innerHTML = `Le joueur ${playerTwo} a gagné !`)
+        : 
+        (message.innerHTML = `Le joueur ${playerOne} a gagné !`)
+        gagne = true;
+      }
+    }
   });
-  
 }
-console.log(combiWin)
-console.log('tableGame :>> ', tableGame);
